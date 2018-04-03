3c5aad259add7d453c3f86576a71dcb3314d6b67
Rasuli Ramin <grantik03@gmail.com>
2018-04-03 14:50:17 +0800

    Все работает

--- app/src/main/java/ru/ya/rrmstu/mywallet/AppContext.java
+++ app/src/main/java/ru/ya/rrmstu/mywallet/AppContext.java
@@ -2,74 +2,69 @@ package ru.ya.rrmstu.mywallet;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Environment;
 import android.util.Log;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public class AppContext extends Application {
 
-    private static final String TAG = AppContext.class.getName();
+    private static final String TAG = Context.class.getName();
 
     private static final String DB_NAME = "wallet.db";
     private static String dbFolder;
     private static String dbPath;
-
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         checkDbExist(this);
-
     }
 
 
-    // если нет файла БД - скопировать его из папки assets
+    /**
+     * Если нет файла БД - скопировать его из папки assets
+     *
+     * @param context
+     */
     private static void checkDbExist(Context context) {
 
-        // определить папку с данными приложения
-//        dbFolder = context.getApplicationInfo().dataDir + "/" + "databases/";
-        dbFolder = "/data/data/" + context.getPackageName() + "/databases/";
+        dbFolder = context.getApplicationInfo().dataDir + "/" + "database/";
+
+        File databaseFolder = new File(dbFolder);
+        if (!databaseFolder.exists()) {
+            databaseFolder.mkdir();
+        }
+
         dbPath = dbFolder + DB_NAME;
 
         if (!checkDataBaseExists()) {
-            System.out.println("!checkDataBaseExists()");
             copyDataBase(context);
         }
     }
 
     private static void copyDataBase(Context context) {
 
-        try (InputStream sourceFile = context.getAssets().open(DB_NAME); // что копируем
-             OutputStream destinationFolder = new FileOutputStream(dbPath); // куда копируем
-        ) {
+        try (InputStream sourceFile = context.getAssets().open(DB_NAME); OutputStream destinationFolder = new FileOutputStream(dbPath)) {
 
-            // создаем папку databases
-            File databaseFolder = new File(dbFolder);
-            databaseFolder.mkdir();
-
-
-            // копируем по байтам весь файл стандартным способом Java I/O
             byte[] buffer = new byte[1024];
             int length;
             while ((length = sourceFile.read(buffer)) > 0) {
                 destinationFolder.write(buffer, 0, length);
-                System.out.println(buffer[buffer.length - length]);
             }
-
         } catch (Exception e) {
             Log.e(TAG, e.getMessage());
         }
-
     }
 
     private static boolean checkDataBaseExists() {
         File dbFile = new File(dbPath);
-        System.out.println(dbFile);
         return dbFile.exists();
     }
 }
