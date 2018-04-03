dfa185c9b18b453ecb23615e092061e2e48469ed
Rasuli Ramin <grantik03@gmail.com>
2018-04-03 20:28:21 +0800

    Добавлен Splash Screen

---
+++ app/src/main/java/ru/ya/rrmstu/mywallet/activities/SplashActivity.java
@@ -0,0 +1,39 @@
+package ru.ya.rrmstu.mywallet.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.ya.rrmstu.mywallet.R;
+import ru.ya.rrmstu.mywallet.app.DbConnection;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Thread() {
+            @Override
+            public void run() {
+                DbConnection.initConnection(getApplicationContext());
+
+                imitateLoading();
+
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }.start();
+    }
+
+    private void imitateLoading() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
